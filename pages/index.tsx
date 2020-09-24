3e9938d931ebd718b48cddaef68b647ec4f41dc9
hkchen <xiaogdgenuine@gmail.com>
2020-09-24 18:14:01 +0800

    Add iphone

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,33 +1,38 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Home.module.scss'
 import {Motion, presets, spring} from "react-motion";
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Keda Manga</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>Keda Manga</title>
+                <link rel="icon" href="/favicon.ico"/>
+            </Head>
 
-      <main className={styles.main}>
-          <Motion defaultStyle={{angle: 0}} style={{angle: spring(-40, presets.noWobble)}}>
-              {({angle}) => <img className={styles.product} src="imgs/ipad.png" alt="" style={{
-                  transform: `rotate(${angle}deg)`
-              }}/>}
-          </Motion>
-      </main>
+            <main className={styles.main}>
+                <Motion defaultStyle={{offsetX: -300}} style={{offsetX: spring(-440, presets.gentle)}}>
+                    {({offsetX}) => (
+                        <div className={styles.product}>
+                            <img className={styles.ipad} src="imgs/ipad.png" alt=""/>
+                            <img className={styles.iphone} src="imgs/iphone.png" alt="" style={{
+                                transform: `translateX(${offsetX}px)`
+                            }}/>
+                        </div>
+                    )}
+                </Motion>
+            </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+            <footer className={styles.footer}>
+                <a
+                    href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Powered by{' '}
+                    <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo}/>
+                </a>
+            </footer>
+        </div>
+    )
 }
